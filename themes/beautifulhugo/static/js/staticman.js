77df5d67ca1d1cd90630e092b9082f69a79e0961
Tiago Gomes <tiago.avv@gmail.com>
2020-05-17 20:03:11 +0100

    Use bootstrap for the modal dialogs

--- themes/beautifulhugo/static/js/staticman.js
+++ themes/beautifulhugo/static/js/staticman.js
@@ -6,35 +6,26 @@
   $('.js-form').submit(function () {
     var form = this;
 
-    $(form).addClass('form--loading');
-
     $.ajax({
       type: $(this).attr('method'),
       url: $(this).attr('action'),
       data: $(this).serialize(),
       contentType: 'application/x-www-form-urlencoded',
       success: function (data) {
-        showModal('Perfect !', 'Thanks for your comment! It will show on the site once it has been approved. .');
-        $(form).removeClass('form--loading');
+        showModal('Perfect!', 'Thanks for your comment! It will show on the site once it has been approved.');
       },
       error: function (err) {
         console.log(err);
         showModal('Error', 'Sorry, there was an error with the submission!');
-        $(form).removeClass('form--loading');
       }
     });
 
     return false;
   });
 
-  $('.js-close-modal').click(function () {
-    $('body').removeClass('show-modal');
-  });
-
   function showModal(title, message) {
-    $('.js-modal-title').text(title);
-    $('.js-modal-text').html(message);
-
-    $('body').addClass('show-modal');
+    $('.modal-title').text(title);
+    $('.modal-body').html(message);
+    $('#staticman-modal').modal();
   }
 })(jQuery);
